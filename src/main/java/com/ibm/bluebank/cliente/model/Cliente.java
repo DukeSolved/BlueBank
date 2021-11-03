7e3a160550caf29691fa23620e47d39ddc315cde
Ezequiel Lorenzatti <ezelorenzatti@gmail.com>
2021-11-03 19:08:26 -0300

    Model Cliente

--- src/main/java/com/ibm/bluebank/cliente/model/Cliente.java
+++ src/main/java/com/ibm/bluebank/cliente/model/Cliente.java
@@ -1,5 +1,44 @@
 package com.ibm.bluebank.cliente.model;
 
+import com.ibm.bluebank.conta.model.Conta;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_CLIENTE")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NOME", nullable = false)
+    private String nome;
+
+    @Column(name = "CPF", nullable = false)
+    private String cpf;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "FONE", nullable = false)
+    private String fone;
+
+    @Column(name = "SENHA", nullable = false)
+    private String senha;
+
+    @Column(name = "RENDA", nullable = false)
+    private Double renda;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Conta.class)
+    @JoinColumn(name = "CONTA_ID")
+    private Conta conta;
 
 }
