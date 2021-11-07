b972643ea06107ee93f7e1cc523ab85673ec1405
ana.dias <anadiasacd690@gmail.com>
2021-11-07 12:14:06 -0300

    ADD:Telefone
    Signed-off-by: ana.dias <anadiasacd690@gmail.com>

--- src/main/java/com/ibm/bluebank/shared/utils/TelefoneUtil.java
+++ src/main/java/com/ibm/bluebank/shared/utils/TelefoneUtil.java
@@ -1,5 +1,21 @@
 package com.ibm.bluebank.shared.utils;
 
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.logging.Logger;
+
 public class TelefoneUtil {
+    public static String TelefoneUtil1(String telefone){
+        MaskFormatter mk;
+        try {
+            mk = new MaskFormatter("(##)#### - ####");
+            mk.setValueContainsLiteralCharacters(false);
+            telefone = mk.valueToString(telefone);
+        }catch (ParseException e){
+            e.printStackTrace();
+            Logger.getLogger("Erro ap aplicar mascara de 8 digitos");
+        }
+        return telefone;
+    }
 
 }
